51a01883a114fbb2e54bcda0c435e2471c2923e4
Ian Johnson <ian@ianjohnson.xyz>
2021-06-26 16:32:52 -0400

    Add CNAME

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = (config) => {
   config.addPlugin(syntaxHighlight);
 
+  config.addPassthroughCopy("CNAME");
   config.addPassthroughCopy("css");
   config.addPassthroughCopy("posts/**/img/*");
   config.addPassthroughCopy("projects/**/img/*");
