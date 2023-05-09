44056917b290b1b6a11a2f0de539e0b8ca8bc6e0
Ian Johnson <ian@ianjohnson.dev>
2023-05-08 21:11:45 -0400

    feat: remove Google Fonts
    System fonts work just fine.

--- _includes/layouts/main.jsx
+++ _includes/layouts/main.jsx
@@ -8,12 +8,6 @@ export default function render({ children, nav, page, title, today }) {
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inconsolata&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-            rel="stylesheet"
-          />
           <link href="/css/index.css" rel="stylesheet" />
           <link href="/css/prism-theme.css" rel="stylesheet" />
           <link rel="icon" type="image/png" href="/favicon.png" />
